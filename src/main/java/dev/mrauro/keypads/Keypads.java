33222b518f53dbbb3af060e88c76a4d53965ec55
Auro <35087590+MrAuro@users.noreply.github.com>
2022-12-23 17:49:42 -0500

    Ensure selected armor stand is a valid keypad

--- src/main/java/dev/mrauro/keypads/Keypads.java
+++ src/main/java/dev/mrauro/keypads/Keypads.java
@@ -30,53 +30,55 @@ public final class Keypads extends JavaPlugin implements Listener {
 
                 String code = armorStand.getCustomName();
 
-                StringBuilder codeBuilder = new StringBuilder();
+                if (code.matches("[1-9]{4}")) {
+                    StringBuilder codeBuilder = new StringBuilder();
 
-                DropperGui gui = new DropperGui("PIN");
+                    DropperGui gui = new DropperGui("PIN");
 
-                gui.setOnGlobalClick(_event -> _event.setCancelled(true));
+                    gui.setOnGlobalClick(_event -> _event.setCancelled(true));
 
-                Label input = new Label(0, 0, 3, 3, Font.LIGHT_GRAY);
+                    Label input = new Label(0, 0, 3, 3, Font.LIGHT_GRAY);
 
-                Player player = event.getPlayer();
+                    Player player = event.getPlayer();
 
-                input.setText("123456789", (character, item) -> new GuiItem(item, _event -> {
-                    codeBuilder.append(character);
+                    input.setText("123456789", (character, item) -> new GuiItem(item, _event -> {
+                        codeBuilder.append(character);
 
-                    if (codeBuilder.length() == 4) {
-                        if (codeBuilder.toString().equals(code)) {
-                            player.getWorld().playSound(player.getLocation(), "block.note_block.pling", 1, 2);
-                            for (int x = -1; x <= 1; x++) {
-                                for (int y = -1; y <= 1; y++) {
-                                    for (int z = -1; z <= 1; z++) {
-                                        Block block = event.getRightClicked().getLocation().getBlock().getRelative(x, y, z);
-                                        if (block.getType() == Material.IRON_DOOR) {
-                                            Door door = (Door) block.getBlockData();
-                                            door.setOpen(true);
-                                            block.setBlockData(door);
-                                            block.getWorld().playSound(block.getLocation(), "block.iron_door.open", 1, 1);
-                                            getServer().getScheduler().scheduleSyncDelayedTask(this, () -> {
-                                                door.setOpen(false);
+                        if (codeBuilder.length() == 4) {
+                            if (codeBuilder.toString().equals(code)) {
+                                player.getWorld().playSound(player.getLocation(), "block.note_block.pling", 1, 2);
+                                for (int x = -1; x <= 1; x++) {
+                                    for (int y = -1; y <= 1; y++) {
+                                        for (int z = -1; z <= 1; z++) {
+                                            Block block = event.getRightClicked().getLocation().getBlock().getRelative(x, y, z);
+                                            if (block.getType() == Material.IRON_DOOR) {
+                                                Door door = (Door) block.getBlockData();
+                                                door.setOpen(true);
                                                 block.setBlockData(door);
-                                                block.getWorld().playSound(block.getLocation(), "block.iron_door.close", 1, 1);
-                                            }, 40L);
+                                                block.getWorld().playSound(block.getLocation(), "block.iron_door.open", 1, 1);
+                                                getServer().getScheduler().scheduleSyncDelayedTask(this, () -> {
+                                                    door.setOpen(false);
+                                                    block.setBlockData(door);
+                                                    block.getWorld().playSound(block.getLocation(), "block.iron_door.close", 1, 1);
+                                                }, 40L);
+                                            }
                                         }
                                     }
                                 }
+                                player.closeInventory();
+                            } else {
+                                player.getWorld().playSound(player.getLocation(), "entity.villager.no", 1, 1);
+                                player.damage(2);
+                                player.sendMessage(ChatColor.RED + "Incorrect code");
+                                player.closeInventory();
                             }
-                            player.closeInventory();
-                        } else {
-                            player.getWorld().playSound(player.getLocation(), "entity.villager.no", 1, 1);
-                            player.damage(2);
-                            player.sendMessage(ChatColor.RED + "Incorrect code");
-                            player.closeInventory();
                         }
-                    }
-                }));
+                    }));
 
-                gui.getContentsComponent().addPane(input);
+                    gui.getContentsComponent().addPane(input);
 
-                gui.show(player);
+                    gui.show(player);
+                }
             }
         }
     }
